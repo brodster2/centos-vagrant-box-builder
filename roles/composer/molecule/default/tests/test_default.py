243c769732c9a0bd448aa24ff51009feb15baa76
Brodie <brodster22@live.com>
2018-10-14 21:48:05 +0100

    setup systemd centos7 docker container

--- roles/composer/molecule/default/tests/test_default.py
+++ roles/composer/molecule/default/tests/test_default.py
@@ -6,7 +6,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
+def test_composer_installed(host):
     composer = host.file('/bin/composer')
 
     assert composer.exists
