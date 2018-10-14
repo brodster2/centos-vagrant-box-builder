43e121209fc0c1edcbbbc798f686fec3c2f431d6
Brodie <brodster22@live.com>
2018-10-14 14:21:26 +0100

    Setup molecule testing in composer role and ignore py env

---
+++ roles/composer/molecule/default/tests/test_default.py
@@ -0,0 +1,15 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    composer = host.file('/bin/composer')
+
+    assert composer.exists
+    assert composer.user == 'root'
+    assert composer.group == 'root'
+    assert composer.mode == 0o775
